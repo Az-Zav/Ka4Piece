c82ad68cc7a04a1678ff4c54abfd997a28ad31e6
Victor Condicion <qvjcondicion@tip.edu.ph>
2026-08-15 23:22:07 +0800

    implemented RankedCandidate and RankedVacancy, fixed Vacancy

--- src/main/java/com/ka4piece/model/Vacancy.java
+++ src/main/java/com/ka4piece/model/Vacancy.java
@@ -38,6 +38,9 @@ public class Vacancy {
     public String getType() {return type;}
     public void setType(String type) {this.type = type;}
 
+    public String getEnteredByOfficialId() {return enteredByOfficialId;}
+    public void setEnteredByOfficialId(String enteredByOfficialId) {this.enteredByOfficialId = enteredByOfficialId;}
+
     public List<String> getSkillRequirements() {return skillRequirements;}
     public void setSkillRequirements(List<String> skillRequirements) {this.skillRequirements = skillRequirements;}
 
