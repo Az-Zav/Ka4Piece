a7081870807c67e4e9528f971db0ac1265cabc41
Victor Condicion <qvjcondicion@tip.edu.ph>
2026-08-15 21:59:29 +0800

    implemented household and barangayofficial models

--- src/main/java/com/ka4piece/model/BarangayOfficial.java
+++ src/main/java/com/ka4piece/model/BarangayOfficial.java
@@ -1,5 +1,26 @@
 package com.ka4piece.model;
 
 public class BarangayOfficial {
-    
+    private String officialID, name, username, password;
+
+    public BarangayOfficial(String officialID, String name, String username, String password) {
+        this.officialID = officialID;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public String getOfficialID() {return officialID;}
+    public void setOfficialID(String officialID) {this.officialID = officialID;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
 }
--- src/main/java/com/ka4piece/model/Household.java
+++ src/main/java/com/ka4piece/model/Household.java
@@ -1,5 +1,36 @@
 package com.ka4piece.model;
 
 public class Household {
-    
+    private String householdId, headName, address, barangay, username, password;
+
+    public Household(String householdId, String headName, String address, String barangay, String username, String password) {
+        this.householdId = householdId;
+        this.headName = headName;
+        this.address = address;
+        this.barangay = barangay;
+        this.username = username;
+        this.password = password;
+    }
+
+    //Getters and setters for each field
+    public String getHouseholdId() {return householdId;}
+    public void setHouseholdId(String householdId) {this.householdId = householdId;}
+
+    public String getHeadName() {return headName;}
+    public void setHeadName(String headName) {this.headName = headName;}
+
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;}
+
+    public String getBarangay() {return barangay;}
+    public void setBarangay(String barangay) {this.barangay = barangay;}   
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+        
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
 }
+
+
